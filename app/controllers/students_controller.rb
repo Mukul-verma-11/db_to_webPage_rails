8b4074fc5e261dd9eb3c26971747dc237468c393
Mukul Verma <mukulverma.it@gmail.com>
2023-06-14 18:43:41 +0530

    only input taken from form and output is shown

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,20 @@
 class StudentsController < ApplicationController
 
     def details
-        @student = Student.find_by(name: params[:id])
+        @student = Student.all
+    end
+
+    def new 
+        @data =  params[:students]
+        print '@data'
+    end
+
+    def create 
+        render plain: params[:student]
+        print '@data'
+        @data =  params[:student]
+        Student.create(name: @data[:name], roll_number: @data[:roll_number])
+        print '@data'
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :students
   # Defines the root path route ("/")
   root "pages#home"
 
-  get "student/:id" , to: "students#details"
+  get "student/new" , to: "students#new"
+  # get "student/show" , to: "students#show"
+
+  get "student/details" , to: "students#details"
+  # get "student" , to: "student#"
 end
